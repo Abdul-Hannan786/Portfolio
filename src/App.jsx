ac4e6f60083d5caa3d9e82549f34487da8c06205
Abdul Hannan <abdulhannanhere@gmail.com>
2025-10-20 20:48:35 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
